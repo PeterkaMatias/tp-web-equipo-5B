401c6ffa4297fbc9afa987e04d318168661d580e
Matias Peterka <matias.tauro@hotmail.com>
2025-05-11 12:11:12 -0300

    Creo estructura base y agrego clases de dominio

---
+++ dominio/Participacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Participacion
+    {
+        public int Id { get; set; }
+        public string CodigoVoucher { get; set; }
+        public int IdPremio { get; set; }
+        public int DniCliente { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ dominio/Voucher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Voucher
+    {
+        public string Codigo { get; set; }
+        public bool Usado { get; set; }
+    }
+}
